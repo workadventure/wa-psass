4f0da229062e81b5d148514e30a7379143b5949d
Hugo Averty <hugoaverty@gmail.com>
2025-04-08 23:08:08 +0200

    Pushing all maps

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,43 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    // Show configuration tile for editors only
-    if (WA.player.tags.includes('admin')) {
-        WA.room.showLayer('exitNorthConfig')
-        WA.room.showLayer('exitSouthConfig')
-        WA.room.showLayer('exitWestConfig')
-        WA.room.showLayer('exitEastConfig')
+    var nightSound = WA.sound.loadSound("night.mp3");
+    var daySound = WA.sound.loadSound("day.mp3");
+    var config = {
+        volume : 1,
+        loop : false,
+        rate : 1,
+        detune : 1,
+        delay : 0,
+        seek : 0,
+        mute : false
     }
+    WA.ui.actionBar.addButton({
+        id: 'night-btn',
+        // @ts-ignore
+        label: 'LA NUIT TOMBE',
+        callback: () => {
+            WA.room.hideLayer('Day');
+            WA.room.hideLayer('DayItems');
+            WA.room.showLayer('Night');
+            WA.room.showLayer('NightItems');
+            daySound.stop();
+            nightSound.play(config);
+        }
+    });
+    WA.ui.actionBar.addButton({
+        id: 'day-btn',
+        // @ts-ignore
+        label: 'LE JOUR SE LÈVE',
+        callback: () => {
+            WA.room.showLayer('Day');
+            WA.room.showLayer('DayItems');
+            WA.room.hideLayer('Night');
+            WA.room.hideLayer('NightItems');
+            nightSound.stop();
+            daySound.play(config);
+        }
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
